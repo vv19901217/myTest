15d08f57a603b150e1cbf5d0bc3d3da0ba830395
wiky <wiky19901217@gmail.com>
2020-05-08 13:13:00 +0800

    关于插件化的一些联系

---
+++ app/src/main/java/cn/com/sinosoft/plugin/MyClassLoader.java
@@ -0,0 +1,16 @@
+package cn.com.sinosoft.plugin;
+
+/**
+ * @ProjectName: CustomViewTest
+ * @Package: cn.com.sinosoft.plugin
+ * @ClassName: MyClassLoader
+ * @Description: 自定义一个类加载的工具类
+ * @Author: wiky
+ * @CreateDate: 2020/5/8 1:12 PM
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2020/5/8 1:12 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class MyClassLoader {
+}
